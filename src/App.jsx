57e03e83ea92e7808e1bf6f2e07614b764e0a968
kdk1026 <daekwang1026@gmail.com>
2025-11-01 14:41:26 +0900

    업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { ErrorBoundary } from "react-error-boundary"
 import CommonRoute from "./components/CommonRoute"
-import { HelmetProvider } from "react-helmet-async"
 import { BrowserRouter } from "react-router-dom"
 import ErrorFallback from "./components/ErrorFallback"
 
 function App() {
 
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <CommonRoute />
-        </ErrorBoundary>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <CommonRoute />
+      </ErrorBoundary>
+    </BrowserRouter>
   )
 }
 
--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,15 +1,14 @@
 import { Outlet } from "react-router-dom";
 import MenuBar from "./MenuBar";
-import { Helmet } from "react-helmet-async";
 
 const MainLayout = () => {
     const title = import.meta.env.VITE_APP_TITLE;
 
     return (
         <>
-            <Helmet>
-                <title>{title}</title>
-            </Helmet>
+            {/* React 19 부터는 react-helmet-async 불필요 */}
+            <meta property="og:title" content={title} />
+            
             <MenuBar />
             <Outlet />
         </>
